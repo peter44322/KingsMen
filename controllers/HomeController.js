0945cc407897e9785d1725744b783a3e0c003795
peter44322 <p.pator@outlook.com>
2018-12-30 07:12:55 +0200

    useless ugly worthless search

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,13 +1,18 @@
 const Post = require('../database/models/Post');
 
 exports.index =async function(req, res) {
-  const  posts = await Post.find({}).populate('user');
+  if(req.query.q){
+    var  posts = await Post.find({title: req.query.q}).populate('user');
+  }else {
+    var  posts = await Post.find({}).populate('user');
+  }
+
+
+
   String.prototype.trunc = String.prototype.trunc ||
         function(n){
             return (this.length > n) ? this.substr(0, n-1) + '&hellip;' : this;
     };
-
-
 
    res.render('index', {
        posts
